46da288f78a064ddb115b69c9ea5f5383ea0bab2
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-02-26 12:06:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-import Fastify from "fastify";
+import { request } from "undici";
+import fastify from "fastify";
 import proxy from "fastify-http-proxy";
-const app = Fastify();
-const PORT = process.env.PORT || 3000, HOST = process.env.HOST || "0.0.0.0";
+const app = fastify();
+const PORT = process.env.PORT || 3000;
+
+app.addHook("preHandler", (_req, reply, done) => {
+    reply.header("Access-Control-Allow-Origin", "*");
+    reply.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    done();
+});
 
 app.register(proxy, { upstream: "https://media.discordapp.net" });
 
@@ -11,4 +18,11 @@ app.register(proxy, {
     rewritePrefix: "/api/adventurer-neutral/"
 });
 
-app.listen(PORT, HOST, () => console.log(`MKChat motd server running on port: ${PORT}`));
+app.get("/lottiesticker/:id", async (req, reply) => {
+    const id = req.params.id;
+    const { body } = await request(`https://discord.com/stickers/${id}.json`);
+    
+    reply.send(body);
+});
+
+app.listen(PORT, process.env.HOST || "0.0.0.0", () => console.log(`MKChat proxy server running on port: ${PORT}`));

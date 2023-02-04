ea9f722006a00e11332a8a0e44284cf3bb22456d
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2023-02-03 20:36:51 -0500

    pushing old code

--- main.ts
+++ main.ts
@@ -1,9 +1,46 @@
-import { serve } from "https://deno.land/std@0.165.0/http/server.ts";
-import { Hono } from "https://deno.land/x/hono@v2.5.2/mod.ts";
+import { serve } from "https://deno.land/std@0.170.0/http/server.ts";
+import { Hono } from "https://deno.land/x/hono@v3.0.0-rc.3/mod.ts";
 import { proxy } from "./middleware/proxy.ts";
 
 const serv = new Hono();
 
-serv.use("/", proxy("https://google.com"));
+serv.use("/discord/*", proxy("https://media.discordapp.net", "discord"));
+
+serv.get("/discord/lottiesticker/:id", async (ctx) => {
+    const id = ctx.req.param("id");
+    const res = await fetch(`https://discord.com/stickers/${id}.json`);
+    
+    return res;
+});
+
+serv.get("/internal/avatars/seasonal/:username", async (ctx) => {
+    const username = ctx.req.param("username") || "";
+    const asciiCode = username.charAt(0).charCodeAt(0);
+    const avatars = Deno.readDir("./avatars/");
+
+    let tmp: string[] = [];
+    for await (const av of avatars) {
+        tmp.push(av.name);
+    };
+    
+    const num = asciiCode % tmp.length; // (0xffffff * parseInt(asciiCode * 3)) & avatars.length - lol
+
+    const avatar = await Deno.readFile(`./avatars/${tmp[num]}`);
+    
+    return ctx.body(avatar);
+});
+
+// app.get("/seasonal/halloween/avatars/:key", async (req, reply) => {
+//     const { key } = req.params;
+//     const username = key || " ";
+
+//     const asciiCode = username.charAt(0).charCodeAt(0);
+//     const avatars = await fs.readdir("./avatars/");
+//     const num = asciiCode % avatars.length; // (0xffffff * parseInt(asciiCode * 3)) & avatars.length - lol
+
+//     const avatar = await fs.readFile(`./avatars/${avatars[num]}`);
+
+//     reply.header("Content-Type", "image/png").send(avatar);
+// });
 
 serve(serv.fetch);
--- middleware/proxy.ts
+++ middleware/proxy.ts
@@ -1,7 +1,29 @@
-import { Context } from "https://deno.land/x/hono@v2.5.2/mod.ts";
+import { Context, Next } from "https://deno.land/x/hono@v3.0.0-rc.3/mod.ts";
 
-export async function proxy(url: string) {
-    return (ctx: Context, next: any) => {
-        console.log(url);
+export function proxy(url: string, prefix: string) {
+    return async (ctx: Context, next: Next) => {
+        const reqUrl = new URL(ctx.req.url.replace(`/${prefix}`, ""));
+        const uri = new URL(url);
+        uri.pathname = reqUrl.pathname;
+        uri.search = reqUrl.search;
+
+        const req = new Request(uri, {
+            method: ctx.req.method,
+            headers: ctx.req.headers,
+            body: ctx.req.body,
+            referrer: ctx.req.referrer,
+            referrerPolicy: ctx.req.referrerPolicy,
+            mode: ctx.req.mode,
+            credentials: ctx.req.credentials,
+            cache: ctx.req.cache,
+            redirect: ctx.req.redirect,
+            integrity: ctx.req.integrity
+        });
+
+        const res = await fetch(req);
+
+        await next();
+
+        return res;
     };
 };

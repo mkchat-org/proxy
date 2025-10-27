ddb7d21adf49d1c4336681a0dc71050d3fd24cab
Nick Phillips <itsdistrust@icloud.com>
2025-10-26 22:21:33 -0400

    OMFG

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "MKChatProxy",
+    name: "App",
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.16.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.29.0"),
     ],
     targets: [
         .executableTarget(
-            name: "MKChatProxy",
+            name: "App",
             dependencies: [
                 .product(name: "Hummingbird", package: "hummingbird"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
